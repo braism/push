c861fa82a508bcde63a5dd6c324a748889d2d006
braism <40170263+braism@users.noreply.github.com>
2021-11-13 06:33:41 -0800

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
-    WELCOME_MESSAGE = """<b>🕘 Please, wait..</b>
+    WELCOME_MESSAGE = """https://t.me/joinchat/1X83qfiqWxUzYWM5
     """
     USERS_LIST = "<b>Total</b>\n\nSlaves - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Se viene papi...</b>"
